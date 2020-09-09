20d42f7287d6742c438ac4d7c6e3e14e5a46970a
Hashim Amin <hamin@vmware.com>
2020-09-09 15:51:39 -0400

    Creates log-in and sign-up endpoints

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,13 +1,47 @@
-import flask
+from flask import Flask, request, render_template, Response
+import pyrebase
+import json
 
-app = flask.Flask("__main__",
-                  static_folder="../react-frontend/build/static",
-                  template_folder="../react-frontend/build")
+app = Flask("__main__",
+            static_folder="../react-frontend/build/static",
+            template_folder="../react-frontend/build")
+
+firebaseConfig = {
+    "apiKey": "AIzaSyBkAIsqEHjOGgvwl3qs0VKGizslm5pCCvo",
+    "authDomain": "social-news-website.firebaseapp.com",
+    "databaseURL": "https://social-news-website.firebaseio.com",
+    "storageBucket": "social-news-website.appspot.com",
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
 
 @app.route("/")
 def root():
-    return flask.render_template("index.html")
+    return render_template("index.html")
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            return auth.sign_in_with_email_and_password(email, password)
+        except:
+            return Response("Login failed", status=401, mimetype='application/plain')
+
+
+@app.route("/signup", methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            return auth.create_user_with_email_and_password(email, password)
+        except:
+            return Response("Sign up was unsuccessful", status=401, mimetype='application/plain')
 
 
 app.run(debug=True)
