84d46b080ba1987a0de99115c0d2103690bfe6a6
Hashim Amin <hamin@vmware.com>
2020-09-11 23:16:16 -0400

    reactifies login component

--- react-frontend/src/components/Login.js
+++ react-frontend/src/components/Login.js
@@ -1,59 +1,126 @@
-import React from "react";
+import React, { Component } from "react";
 
-export const Login = () => {
-  return (
-    <div>
-        <b>Email</b>
-        <br/>
-        <br/>
-        <form method="post" action="/rest/login">
-            <table border="0">
-                <tbody>
-                    <tr>
-                        <td>username:</td>
-                        <td>
-                            <input type="email" name="email" size="20" autocorrect="off" spellcheck="false" autocapitalize="off" autofocus="true"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>password:</td>
-                        <td>
-                            <input type="password" name="password" size="20"/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+const url = '/rest/login'
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            account: {},
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    handleSubmitLogin = (e) => {
+        e.preventDefault();
+        var formData = new FormData();
+        formData.append('email', this.state.email);
+        formData.append('password', this.state.password);
+
+        const requestOptions = {
+            method: 'POST',
+            body: formData
+        };
+
+        fetch('/rest/login', requestOptions)
+            .then(response => {
+                console.log(response.status);
+                console.log(response.data);
+                this.setState({account: response.data});
+                return response.json();
+            })
+            .catch(function(error) {
+                console.log('Request failed', error)
+            });
+    }
+
+    handleSubmitSignup = (e) => {
+        e.preventDefault();
+        var formData = new FormData();
+        formData.append('email', this.state.email);
+        formData.append('password', this.state.password);
+
+        const requestOptions = {
+            method: 'POST',
+            body: formData
+        };
+
+        fetch('/rest/signup', requestOptions)
+            .then(response => {
+                console.log(response.status);
+                console.log(response.data);
+                this.setState({account: response.data});
+                return response.json();
+            })
+            .catch(function(error) {
+                console.log('Request failed', error)
+            });
+    }
+
+    render() {
+     const email = this.state.email;
+     const password = this.state.password;
+     return (  <div>
+            <b>Email</b>
             <br/>
-            <input type="submit" value="login"/>
-        </form>
-        <a href="forgot">Forgot your password?</a>
-        <br/>
-        <br/>
-        <b>Create Account</b>
-        <br/>
-        <br/>
-        <form method="post" action="/rest/signup">
-            <table border="0">
-                <tbody>
-                    <tr>
-                        <td>username:</td>
-                        <td>
-                            <input type="email" name="email" size="20" autocorrect="off" spellcheck="false" autocapitalize="off"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>password:</td>
-                        <td>
-                            <input type="password" name="password" size="20"/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
             <br/>
-            <input type="submit" value="create account"/>
-        </form>
-    </div>
-  );
-};
+            <form onSubmit={this.handleSubmitLogin}>
+                <table border="0">
+                    <tbody>
+                        <tr>
+                            <td>username:</td>
+                            <td>
+                                <input type="email" name="email" size="20" autocorrect="off" spellcheck="false"
+                                       autocapitalize="off" value={email} onChange={this.handleChange}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>password:</td>
+                            <td>
+                                <input type="password" name="password" size="20"
+                                       required value={password} onChange={this.handleChange}/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br/>
+                <input type="submit" value="login"/>
+            </form>
+            <a href="forgot">Forgot your password?</a>
+            <br/>
+            <br/>
+            <b>Create Account</b>
+            <br/>
+            <br/>
+            <form onSubmit={this.handleSubmitSignup}>
+                <table border="0">
+                    <tbody>
+                        <tr>
+                            <td>username:</td>
+                            <td>
+                                <input type="email" name="email" size="20" autocorrect="off" spellcheck="false"
+                                       autocapitalize="off" value={email} onChange={this.handleChange}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>password:</td>
+                            <td>
+                                <input type="password" name="password" size="20"
+                                       required value={password} onChange={this.handleChange}/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <br/>
+                <input type="submit" value="create account"/>
+            </form>
+        </div>);
+    }
+}
 
 export default Login;
